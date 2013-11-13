55c79cb813d987037a315f6a3ed3b044a392c507
taiyang521hao <taiyang521hao@163.com>
2013-11-13 11:17:15 +0800

    Initial Commit

---
+++ MiuTrip/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MiuTrip
+//
+//  Created by SuperAdmin on 11/13/13.
+//  Copyright (c) 2013 michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MiuTripTests/MiuTripTests.m
@@ -0,0 +1,34 @@
+//
+//  MiuTripTests.m
+//  MiuTripTests
+//
+//  Created by SuperAdmin on 11/13/13.
+//  Copyright (c) 2013 michael. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MiuTripTests : XCTestCase
+
+@end
+
+@implementation MiuTripTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
